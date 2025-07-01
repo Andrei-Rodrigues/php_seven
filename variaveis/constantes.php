9dca16daa21c58911612caa5d1a92271dd143fc6
Andrei-Rodrigues <andreigeral86@gmail.com>
2025-06-30 21:39:03 -0300

    Estudo de constantes, finalizando secao 5

---
+++ variaveis/constantes.php
@@ -0,0 +1,15 @@
+<div class="titulo">Constantes</div>
+
+<?php
+
+define('TAXA_DE_JUROS', 5.9);
+echo TAXA_DE_JUROS;
+
+const NOVA_TAXA = 2.5;
+echo "<br>" . NOVA_TAXA;
+
+$valorVariavel = 2.8;
+define('NOVISSIMA_TAXA', $valorVariavel);
+echo '<br>' . NOVISSIMA_TAXA;
+
+echo '<br>' . PHP_VERSION;
