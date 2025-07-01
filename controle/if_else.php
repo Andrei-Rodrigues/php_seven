78a5032d37c24feb2471cbba3747d6764066a8cc
Andrei-Rodrigues <andreigeral86@gmail.com>
2025-06-30 23:15:32 -0300

    if else secao 6

---
+++ controle/if_else.php
@@ -0,0 +1,37 @@
+<div class="titulo">If Else</div>
+
+<?php
+
+if(true){
+    echo "Serei impresso?<br>";
+}
+
+if(false){
+    echo "Verdadeiro<br>";
+} else {
+    echo "Falso<br>";
+}
+
+if(false){
+    
+} else if(false) {
+
+} else {
+    if(false) {
+        
+    } else {
+        echo "Aqui<br>";
+    }
+}
+
+if(false){
+    echo "Passo A<br>";
+} else if (false){
+    echo "Passo B<br>";
+} else if(true){
+    echo "Passo C<br>";
+} else {
+    echo "Último passo<br>";
+}
+
+echo "Fim<br>";
