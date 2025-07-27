89ebdc09cdeae52a1f6f097d01fc60b19a7c8c5b
Andrei-Rodrigues <andreigeral86@gmail.com>
2025-07-27 10:19:52 -0300

    Estudo de função e escopo, PHP 7/seção 9

---
+++ funcoes/escopo.php
@@ -0,0 +1,33 @@
+<div class="titulo">Função escopo</div>
+
+<?php
+
+function imprimirMensagens() {
+    echo "Olá";
+    echo "até a próxima<br>";
+}
+
+imprimirMensagens();
+imprimirMensagens();
+imprimirMensagens();
+
+$variavel = 1;
+
+function trocaValor() {
+    $variavel = 2;
+    echo "Durante a função: $variavel<br>";
+}
+
+echo "Antes da função: $variavel<br>";
+trocaValor();
+echo "Depois da função: $variavel<br>";
+
+function trocaValorDeVerdade() {
+    global $variavel;
+    $variavel = 3;
+    echo "Durante a função: $variavel<br>";
+}
+
+echo "Antes da função: $varialvel<br>";
+trocaValorDeVerdade();
+echo "Depois da função: $variavel<br>";
