d7c0c67fa8977ca79783ab4a9dc9a0fa3aefaaa6
Andrei-Rodrigues <andreigeral86@gmail.com>
2025-11-12 22:15:50 -0300

    Estudo de classe abstrata

---
+++ classes_objetos/abstract.php
@@ -0,0 +1,39 @@
+<div class="titulo">Classe Abstrata</div>
+
+<?php 
+
+abstract class Abstrata {
+    abstract public function metodo1();
+    abstract protected function metodo2($parametro);
+}
+
+abstract class FilhaAbstrata extends Abstrata {
+    public function metodo1() {
+        echo "executando metodo 1 <br>";
+    }
+
+    abstract public function metodo3();
+}
+
+class Concreta extends FilhaAbstrata {
+    public function metodo1() {
+        echo "executando metodo 1 extendido <br>";
+        parent::metodo1();
+    }
+
+    protected function metodo2($parametro) {
+        echo "executando metodo 2 com $parametro <br>";
+    }
+
+    public function metodo3() {
+        echo "executando metodo 3 extendido <br>";
+        $this->metodo2('interno');
+    }
+        
+}
+
+$c = new Concreta();
+$c->metodo1();
+$c->metodo3();
+echo "<br>";
+var_dump($c instanceof Concreta);
